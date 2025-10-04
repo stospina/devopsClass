7575d43360cf9907708dc509af039298d836ea55
Steven.Ospina <steven.ospina@jalasoft.com>
2025-10-04 13:37:45 -0500

    adding steps to run self-hosted

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hola mundo, esto es un test de la clase DevOps"
 
+@app.get("/home")
+def get_home():
+    return home()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
